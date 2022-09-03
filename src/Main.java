d98524fd453eb17b99bf5f6280a42c247906474d
ridma <ridmakaveendra7@gmail.com>
2022-09-03 23:59:54 +0530

    sec

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MaxMinFinder finder = new MaxMinFinder();
+
+        int [] arr = {-10,25,50,60,-40,80,-22};
+
+        finder.findMaxMin(arr);
+
     }
 }
--- src/MaxMinFinder.java
+++ src/MaxMinFinder.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class MaxMinFinder {
+public class MaxMinFinder {
+    int max;
+    int min;
+
+    public void findMaxMin(int[] array) {
+
+        max = array[0];
+        min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+
+
+        }
+        System.out.println("max = "+max);
+        System.out.println("min = "+min);
+
+    }
 }
